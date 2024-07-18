26dc42230f00af03f346307728da2bcd708d49d3
markus583 <markus.frohmann@gmail.com>
2024-07-18 11:08:52 +0200

    add metadata; "shard-"

--- v2d_to_metadata.py
+++ v2d_to_metadata.py
@@ -6,21 +6,24 @@ import tarfile
 import tempfile
 from datetime import timedelta
 
-
-def timestamp_to_frames(timestamp, fps):
-    """Converts a timestamp in the format 'HH:MM:SS.mss' into a frame count."""
-    # TODO: check correctness of this conversion
-    hrs, mins, secs = map(float, timestamp.split(":"))
-    total_seconds = timedelta(hours=hrs, minutes=mins, seconds=secs).total_seconds()
-    # TODO: is round the right way of doing this? Most transcripts are assigned to only 1-2 frames...
-    return round(total_seconds * fps)
+# FIXME: may need adaptation
+METADATA_MAPPING = {
+    "webpage_url": "url",
+    "title": "title",
+    "duration": "duration",
+    "channel": "channel",
+    "fps": "fps",
+    "tags": "tags",
+    "resolution": "resolution",
+    "aspect_ratio": "aspect_ratio",
+}
 
 
 def process_tar_files(source_directory, target_directory, skip_existing=True):
     """Extract, process, and re-package JSON files in TAR archives."""
     # TODO: this path
     # source_directory = os.path.join(source_directory, "video_rgb")
-    target_directory = os.path.join(target_directory, "transcripts")
+    target_directory = os.path.join(target_directory, "metadata")
 
     os.makedirs(target_directory, exist_ok=True)
 
@@ -49,7 +52,7 @@ def process_tar_files(source_directory, target_directory, skip_existing=True):
                     with tarfile.open(target_tar_path, "w") as out_tar:
                         for root, dirs, files in os.walk(temp_dir):
                             for file in files:
-                                if file.endswith(".jsonl"):
+                                if file.endswith(".json"):
                                     out_tar.add(os.path.join(root, file), arcname=file)
                 finally:
                     shutil.rmtree(temp_dir)
@@ -59,35 +62,30 @@ def process_json_file(json_file_path, output_dir):
     """Reads and processes a single JSON file to convert it to the required format."""
     with open(json_file_path, "r", encoding="utf-8") as file:
         data = json.load(file)
+        # remove filepath of json
+        os.remove(json_file_path)
         video_key = os.path.splitext(os.path.basename(json_file_path))[0]
+        
+        json_content = {}
 
         if data["status"] != "success":
             # errored while downloading
             return
         elif "subtitles" not in data["yt_meta_dict"]:
             print(data)
-            # TODO: what to do with videos that have no subtitles? When can this occur?
+            # XXX: always ensure to only write metadata where we have everything we need
+            # (transcript, video, ...)
             return
         if data["yt_meta_dict"]["subtitles"].keys() != {"en"}:
             # XXX: for now, we decided to only exclude non-English videos
             return
-        subtitles = data["yt_meta_dict"]["subtitles"]["en"]
-        fps = data["yt_meta_dict"]["info"]["fps"]
-
-        json_content = []
-        for subtitle in subtitles:
-            start_frame = timestamp_to_frames(subtitle["start"], fps)
-            end_frame = timestamp_to_frames(subtitle["end"], fps)
-            json_content.append(
-                {
-                    "transcript": " ".join(subtitle["lines"]),
-                    "start_frame_index": start_frame,
-                    "end_frame_index": end_frame,
-                }
-            )
-
-        jsonl_filename = f"{video_key}.jsonl"
-        with open(os.path.join(output_dir, jsonl_filename), "w") as outfile:
+        for key, value in METADATA_MAPPING.items():
+            if value in data["yt_meta_dict"]["info"]:
+                json_content[key] = data["yt_meta_dict"]["info"][value]
+        
+        json_filename = f"{video_key}.json"
+        with open(os.path.join(output_dir, json_filename), "w") as outfile:
+            print(outfile)
             json.dump(json_content, outfile, indent=4)
 
 
